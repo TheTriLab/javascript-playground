92b9686a018e8cd16dd09f1d03c430a495275783
triduong <mtworkweb@gmail.com>
2024-01-04 22:45:25 +0700

    Expense Tracker - Add new transaction to the list

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -16,13 +16,42 @@ function addTransaction(e) {
     } else if (!transactionAmount) {
         alert('Please add a amount');
     } else {
-        transactionHistoryItems.push(
-            {
-                title: transactionTitle,
-                amount: transactionAmount
-            }
-        )
+        const newItem = {
+            title: transactionTitle.trim(),
+            amount: transactionAmount.trim()
+        }
+
+        transactionHistoryItems.push(newItem)
+
+        addTransactionDOM(newItem)
+
+        // Reset inputs after item is added to the list
+        e.target.reset()
     }
+}
+
+// Add transaction to DOM list
+function addTransactionDOM(transaction) {
+    // Get sign
+    const sign = transaction.amount < 0 ? '-' : '+'
+
+    // Create history item
+    const item = document.createElement('div')
+    item.classList.add(
+        'history-item',
+        'card',
+        'small-card',
+        transaction.amount < 0 ? 'history-minus' : 'history-plus'
+    )
+
+    item.innerHTML = `
+        <span class="close">x</span>
+        <span>${transaction.title}</span>
+        <span>${sign}${Math.abs(transaction.amount)}</span>
+    `
+    // Insert new transaction items to history UI
+    const historySectionContent = document.getElementById('transaction-history')
+    historySectionContent.appendChild(item)
 }
 
 form.addEventListener('submit', addTransaction)
