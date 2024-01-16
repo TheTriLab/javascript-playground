613fbc97b5d944cab5453fb82277d2b6dd7e98ad
triduong <mtworkweb@gmail.com>
2024-01-16 23:17:37 +0700

    Music Player - Play and pause for disc

--- music-player/script.js
+++ music-player/script.js
@@ -1,12 +1,20 @@
+const playerContainer = document.getElementById('player')
 const playBtn = document.getElementById('play')
+const playIcon = playBtn.querySelector('i')
+const discEl = document.getElementById('disc')
+
+
 let isPlay = true
 
 
 playBtn.addEventListener('click', (e) => {
+    // Toggle play class for player
+    playerContainer.classList.toggle('play')
     
+    // Toggle play icon
+    playIcon.classList.toggle('fa-play')
+    playIcon.classList.toggle('fa-pause')
+
     // Toggle play status
-    const playBtnClasses = isPlay ? 'fas fa-pause' : 'fas fa-play'
-    e.target.classList = playBtnClasses
     isPlay = !isPlay
-    
 })
