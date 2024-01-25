938ce7f21785a421935e0bdb6981473dcd7b79be
triduong <mtworkweb@gmail.com>
2024-01-25 22:49:56 +0700

    Music Player - Add next, prev function

--- music-player/script.js
+++ music-player/script.js
@@ -2,13 +2,29 @@ const playerContainer = document.getElementById('player')
 const playBtn = document.getElementById('play')
 const playIcon = playBtn.querySelector('i')
 const nextBtn = document.getElementById('next')
-const prevBtn = document.getElementById('prev')
+const prevBtn = document.getElementById('previous')
 const discEl = document.getElementById('disc')
 const audioEl = document.getElementById('audio')
 
+const audioPlayList = [
+    {
+        name: 'ukulele',
+        audioUrl: './assets/music/ukulele.mp3',
+        avatarUrl: './assets/img/ukulele.jpg',
+    },
+    {
+        name: 'summer',
+        audioUrl: './assets/music/summer.mp3',
+        avatarUrl: './assets/img/summer.jpg',
+    },
+    {
+        name: 'hey',
+        audioUrl: './assets/music/hey.mp3',
+        avatarUrl: './assets/img/hey.jpg',
+    },
+]
 
-
-
+let currentAudio = audioPlayList[0]
 
 playBtn.addEventListener('click', () => {
     // Check the audio play status
@@ -29,12 +45,38 @@ playBtn.addEventListener('click', () => {
     }
 })
 
+function loadAudio() {
+    // Check the audio play status
+    const isPlay = !audioEl.paused && audioEl.currentTime > 0
+
+    // Load audio info
+    audioEl.setAttribute('src', currentAudio.audioUrl)
+    discEl.setAttribute('src', currentAudio.avatarUrl)
+    discEl.setAttribute('alt', currentAudio.name)
+
+    // Play audio
+    if (isPlay) {
+        audioEl.play()
+    } else {
+        audioEl.pause()
+    }
+}
+
 // Click next button
 nextBtn.addEventListener('click', () => {
+    const currentAudioIndex = audioPlayList.findIndex(e => e.name === currentAudio.name)
+    const nextAudioIndex = currentAudioIndex < audioPlayList.length - 1 ? currentAudioIndex + 1 : 0
+    currentAudio = audioPlayList[nextAudioIndex]
 
+    loadAudio()
 })
 
 // Click previous button
 prevBtn.addEventListener('click', () => {
+    const currentAudioIndex = audioPlayList.findIndex(e => e.name === currentAudio.name)
+    const previousAudioIndex = currentAudioIndex > 0 ? currentAudioIndex - 1 : audioPlayList.length - 1
+    currentAudio = audioPlayList[previousAudioIndex]
     
+    loadAudio()
 })
+
